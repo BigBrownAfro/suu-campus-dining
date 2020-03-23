9435a32e8eacb0814bb4fb7b5c4b63614ff3647b
BigBrownAfro <bigbrownafro@gmail.com>
2020-03-23 00:51:13 -0600

    Created Components for most of the main pages. Created the html and css for the login page. No actual functionality.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SidebarModule } from 'ng-sidebar';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { LoginComponent } from './login/login.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { HistoryComponent } from './history/history.component';
+import { MenuComponent } from './menu/menu.component';
+
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'history', component: HistoryComponent},
+  {path: 'favorites', component: FavoritesComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'}
+]
 
 @NgModule({
-  declarations: [AppComponent, SideNavComponent],
-  imports: [BrowserModule, SidebarModule.forRoot()],
+  declarations: [
+    AppComponent, 
+    SideNavComponent, 
+    LoginComponent, 
+    FavoritesComponent, 
+    HistoryComponent, 
+    MenuComponent
+  ],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule, 
+    SidebarModule.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
